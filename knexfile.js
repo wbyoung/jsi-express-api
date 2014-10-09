63adee19e8966a48f29f4467ec961df78e9ae9ce
Whitney Young <wbyoung@fadingred.com>
2014-10-09 13:55:26 -0700

    First test.

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,16 @@ module.exports = {
     }
   },
 
+  testing: {
+    client: 'postgres',
+    connection: {
+      host     : process.env.APP_DB_HOST     || '127.0.0.1',
+      user     : process.env.APP_DB_USER     || '',
+      password : process.env.APP_DB_PASSWORD || '',
+      database : process.env.APP_DB_NAME     || 'people_test'
+    }
+  },
+
   staging: {
     client: 'postgresql',
     connection: {
---
+++ test/server_tests.js
@@ -0,0 +1,54 @@
+'use strict';
+
+process.env.NODE_ENV = 'testing';
+// NODE_ENV=testing ./node_modules/.bin/knex migrate:latest
+
+var _ = require('lodash');
+var expect = require('chai').expect;
+var request = require('request');
+var util = require('util');
+var server = require('../server');
+var app = server.app;
+var knex = server.knex;
+var Person = server.Person;
+var server;
+var port = 38239;
+var baseURL = util.format('http://localhost:%d', port);
+
+
+describe('api', function() {
+
+  before(function(done) {
+    server = app.listen(port, done);
+  });
+
+  after(function(done) {
+    server.close(done);
+  });
+
+  beforeEach(function(done) {
+    knex('people').delete().then(function() { done(); }, done);
+  });
+
+  it('handles POST /api/people', function(done) {
+    var data = {
+      firstName: 'Whitney',
+      lastName: 'Young',
+      address: 'Chicago'
+    };
+    request.post(baseURL + '/api/people', { form: data }, function(err, response, body) {
+      Person.fetchAll().then(function(people) {
+        var peopleWithoutIds = people.toJSON().map(function(person) {
+          return _.omit(person, 'id');
+        });
+        expect(peopleWithoutIds).to.eql([{
+          firstName: 'Whitney',
+          lastName: 'Young',
+          address: 'Chicago'
+        }]);
+        done();
+      });
+    });
+  });
+
+});

8ed6f38341979f19b7828cab11d105b04f0c1d1b
Whitney Young <wbyoung@fadingred.com>
2014-10-09 13:24:11 -0700

    Whitespace.

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
       user     : process.env.APP_DB_USER     || '',
       password : process.env.APP_DB_PASSWORD || '',
       database : process.env.APP_DB_NAME     || 'people'
-}
+    }
   },
 
   staging: {
